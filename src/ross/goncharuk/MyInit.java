fde228f094b70fbefec0983a3cf28cbaf701ddbe
Ross <rgoncharuk@gmail.com>
2016-02-22 20:43:47 +0200

    Upd with for loop

--- src/ross/goncharuk/MyInit.java
+++ src/ross/goncharuk/MyInit.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public class MyInit {
     /*in 2.5.3 changing the field "arr" to static won't change the result as
-    arr will be initialized at non-static block. Intellij Idea will suggest changing non-static block to static*/
+    arr will be initialized at non-static block, yet Intellij Idea will suggest changing non-static block to static*/
     private static int [] arr;
-    {
+    /*in 2.5.4 changing initialization block to static won't change the result as
+    static block will be initialized right after declaring int[] arr*/
+    static {
         arr = new int[10];
         for(int i = 0;i<=9;i++){
             arr[i]=(int) (10 * Math.random());
